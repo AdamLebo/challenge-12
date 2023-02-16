b07c0509bb62ccd263cc98c2c064126ebda2fae8
AdamLebo <alieberman10@aol.com>
2023-02-16 13:56:57 -0500

    added schema

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "employees"
+  database: "employeesDB"
 });
 
 connection.connect(function (err) {
